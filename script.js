cf02fc8ae7e76e1237b6752ab1e6cfa1356d0cac
KSRS CHARAN <51869689+ksrscharan@users.noreply.github.com>
2024-11-11 13:05:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
-const listItems = document.querySelectorAll('.list-item')
-const lists = document.querySelectorAll('.list')
-let draggedItem = null
-for(let i=0; i<listItems.length;i++){
-    const item = listItems[i]
-    item.addEventListener("dragstart", ()=>{
-        draggedItem = item
-        setTimeout(()=>{
-            item.style.display = 'none'
-        }, 0)
-    })
-    item.addEventListener("dragend", ()=>{
-        draggedItem.style.display = 'block'
-        setTimeout(()=>{
-            draggedItem = null
-        }, 0)
-    })
-    for(let j=0; j<lists.length; j++){
-        const list = lists[j]
-        list.addEventListener('dragover', (e)=>{
-            e.preventDefault()
-            list.style.backgroundColor = 'rgba(0,0,0, 0.2)'
-        })
-        list.addEventListener('dragenter', (e)=>{
-            e.preventDefault()
-        })
-        list.addEventListener('dragleave', ()=>{
-            list.style.backgroundColor = 'rgba(0,0,0, 0.1)'
-        })
-        list.addEventListener('drop', ()=>{
-            list.append(draggedItem)
-            list.style.backgroundColor = 'rgba(0,0,0, 0.1)'
-        })
-    }
-}  
+const draggables = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging');
+    });
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging');
+    });
+});
+
+lists.forEach(list => {
+    list.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const dragging = document.querySelector('.dragging');
+        list.appendChild(dragging);
+    });
+});
